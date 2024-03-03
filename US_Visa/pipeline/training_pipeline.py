87d6c3391b1d87882c2321f35366844e8ee5266f
judirispah <judi.rispah.123@gmail.com>
2024-03-04 02:00:43 +0530

    validation done

--- US_Visa/pipeline/training_pipeline.py
+++ US_Visa/pipeline/training_pipeline.py
@@ -1,16 +1,17 @@
 import sys
 from us_visa.Exception import USvisaException
 from us_visa.logger import logging
-
+from us_visa.Components.data_validation import DataValidation
 from us_visa.Components.data_ingestion import DataIngestion
-from us_visa.entity.config_entity import (DataIngestionConfig)
+from us_visa.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
                                           
 
-from us_visa.entity.artifact_entity import (DataIngestionArtifact)
+from us_visa.entity.artifact_entity import (DataIngestionArtifact,DataValidationArtifact)
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config=DataValidationConfig()
 
     def start_data_ingestion(self)-> DataIngestionArtifact:
          
@@ -30,13 +31,32 @@ class TrainPipeline:
             raise USvisaException(e, sys) from e
         
 
+
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
+
+        try:
+            logging.info("Entered the start_data_validation method of TrainPipeline class")
+            data_validation=DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=self.data_validation_config)
+            data_validation_artifact=data_validation.initiate_data_validation()
+
+            logging.info("Exited the start_data_validation method of TrainPipeline class")
+            return data_validation_artifact,
+        
+        except Exception as e:
+            raise USvisaException(e, sys) from e
+        
+        
+
           
-    def run_pipeline(self, ) -> None:
+    def run_pipeline(self) -> None:
         """
         This method of TrainPipeline class is responsible for running complete pipeline
         """
         try:
-            run = self.start_data_ingestion()
+            
+            data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            
 
 
         
